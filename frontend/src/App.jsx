7201dc43d3f1455818673706f6ec51c92125f172
devEduardoReis <rodrigueseduardoreis@gmail.com>
2026-05-24 17:05:56 -0300

    feat: adiciona página Início (landing page)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Inicio from './pages/Inicio';
 import ProjetosLei from './pages/ProjetosLei';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<ProjetosLei />} />
+        <Route path="/" element={<Inicio />} />
         <Route path="/projetos" element={<ProjetosLei />} />
       </Routes>
     </BrowserRouter>
